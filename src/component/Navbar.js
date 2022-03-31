7b0ed6cd349fe5395d521aa574dfd694d27f86d4
unknown <dwivediom00@gmail.com>
2022-03-31 14:08:16 +0530

    search removed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -34,7 +34,10 @@ export default function Navbar(props) {
                   About us 
                 </Link>
               </li>
-             <div className="mx-3"  onClick={props.changemode}>
+             
+            </ul>
+
+            <div className="mx-3"  onClick={props.changemode}>
              <label className="theme">
                 <span>Light</span>
                 <span className="theme__toggle-wrap">
@@ -62,19 +65,6 @@ export default function Navbar(props) {
                 <span>Dark</span>
               </label>
              </div>
-            </ul>
-
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
